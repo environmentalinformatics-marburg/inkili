f93ca2a357811ca870229cb360df1e2b03fddae1
Alice Ziegler <aziegler@pc19ui157.Geographie.Uni-Marburg.DE>
2015-12-16 14:18:36 +0100

    ldr_query update with new variables

--- src/ldr_query.R
+++ src/ldr_query.R
@@ -44,6 +44,8 @@ ldr_query <- function(plotID, crdnt_x, crdnt_y, radius){
     ldr_mdn_rtrn <- median(ldr_pnts_all$returns)
     #calculate maximum scan angle within plot
     ldr_max_angl <- max(abs(ldr_pnts_all$scanAngleRank))
+    #calculate standard deviation of all first returns in one plot
+    ldr_sd_firsts <- sd(ldr_pnts_all$z[which(ldr_pnts_all$returnNumber == 1)])
     # calculate quantiles # changed: from Stefan
     data <- ldr_pnts_all$z
     qmin <- quantile(data, 0.01)
@@ -70,6 +72,7 @@ ldr_query <- function(plotID, crdnt_x, crdnt_y, radius){
       cffnt_x3 <- NA
       cffnt_x4 <- NA
     } else {
+      ###get 4 coefficients to describe density function
     ldr_dnst <- density(ldr_pnts_all$z)
     dnst <- data.frame(cbind(x=ldr_dnst$x, y=ldr_dnst$y))
     fnct_dnst <- glm(formula = y ~ x + I(x^2) + I(x^3) + I(x^4), data = dnst)
@@ -81,17 +84,18 @@ ldr_query <- function(plotID, crdnt_x, crdnt_y, radius){
     cffnt_x4 <- smmry_dnst$coefficients[5,1]
     }
     return(list(max_hght = ldr_max_hght, sd = ldr_sd_hght, mdn = ldr_mdn_rtrn,
-                max_angl = ldr_max_angl, qntl_0 = ldr_qntl_0,
-                qntl_25 = ldr_qntl_25, qntl_50 = ldr_qntl_50,
-                qntl_75 = ldr_qntl_75, qntl_100 = ldr_qntl_100, 
-                cffnt_intcpt = cffnt_intcpt, cffnt_x = cffnt_x, 
-                cffnt_x2 = cffnt_x2, cffnt_x3 = cffnt_x3, cffnt_x4 = cffnt_x4))
+                max_angl = ldr_max_angl, sd_firsts = ldr_sd_firsts, 
+                qntl_0 = ldr_qntl_0, qntl_25 = ldr_qntl_25, 
+                qntl_50 = ldr_qntl_50, qntl_75 = ldr_qntl_75, 
+                qntl_100 = ldr_qntl_100, cffnt_intcpt = cffnt_intcpt, 
+                cffnt_x = cffnt_x, cffnt_x2 = cffnt_x2, cffnt_x3 = cffnt_x3, 
+                cffnt_x4 = cffnt_x4))
   })
   vars <- as.data.frame(t(ldr_sapply))
   ### unlist elements from sapply-loop
-#   for (i in 1:ncol(vars)) {
-#     vars[, i] <- unlist(vars[, i])
-#     }
+  for (i in 1:ncol(vars)) {
+    vars[, i] <- unlist(vars[, i])
+    }
   ldr_var <- cbind(plotID, crdnt_x, crdnt_y, vars)
 
   return(ldr_var)
