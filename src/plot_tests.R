dfd7aee763c7c7a882f414d415adf712f0b194f5
Alice Ziegler <aziegler@pc19ui157.Geographie.Uni-Marburg.DE>
2016-01-27 15:49:03 +0100

    delete unused files

--- src/plot_tests.R
+++
@@ -1,9 +0,0 @@
-ggplot(lst_models, aes(x = testing_response, y = testing_predicted, color = model_response)) +
-  geom_point()
-
-ggplot(lst_models[lst_models$model_response == "Acari",], aes(x = testing_response, y = testing_predicted, color = model_response)) +
-  geom_point()
-
-plot(testing_predicted ~ testing_response, lst_models[lst_models$model_response == "total_insct",])
-abline(lm(testing_predicted ~ testing_response, lst_models[lst_models$model_response == "total_insct",]))
-summary(lm(testing_predicted ~ testing_response, lst_models[lst_models$model_response == "total_insct",]))
