d309e02bce43aa7b916084f7c8584b8e9928c564
Alice Ziegler <aziegler@pc19ui157.Geographie.Uni-Marburg.DE>
2016-04-04 16:13:01 +0200

    update after master thesis clear out

--- src/auto_model.R
+++
@@ -1,5 +0,0 @@
-list.files(/media/aziegler/Volume/data_div)
-
-(path = ".", pattern = NULL, all.files = FALSE,
-full.names = FALSE, recursive = FALSE,
-ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
